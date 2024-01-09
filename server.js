5eedf57f2bb9d1dda87fe243ec752af0057da48c
Eren <eren.bicer@hotmail.com>
2024-01-09 18:23:35 +0300

    Created new MySQL database and established successful connection from Node.js application

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
+const bluebird = require('bluebird');
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -10,62 +12,68 @@ app.use(
     extended: true,
   })
 );
+
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (insecure, use a specific origin in production)
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// get the client
-const mysql = require('mysql2');
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
+const config = {
+  host: 'db',
   user: 'root',
   database: 'test',
-  password: 'Admin123!'
-});
+  password: 'Admin123'
+};
 
+let connection;
 
+const connectWithRetry = async () => {
+  try {
+    connection = await mysql.createConnection(config);
+    console.log('Successfully connected to the database.');
+  } catch (err) {
+    console.log('Unable to connect to the database. Retrying in 5 seconds...');
+    console.error(err)
+    await bluebird.delay(5000);
+    return connectWithRetry();
+  }
 
-// Define a route to retrieve all users
-app.get('/all', (req, res) => {
-  // Select all users from the MySQL database
+  // Define a route to retrieve all users
+  app.get('/all', (req, res) => {
+    connection.query(
+      'SELECT * FROM `user`',
+      function(err, results, fields) {
+        console.log(results); // results contains rows returned by server
+        res.status(200).json(results);
+      }
+    ) 
+  });
 
-  connection.query(
-    'SELECT * FROM `user`',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      res.status(200).json(results);
-    }
-  ) 
-});
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Replace 'index.html' with your actual HTML file path
-});
+  app.get('/health', (req, res) => {
+    res.sendStatus(200);
+  });
 
-app.get('/health', (req, res) => {
-  res.sendStatus(200);
-});
+  app.post('/add', (req, res) => {
+    const { name, surname, email, password } = req.query;
 
-app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`);
-});
+    connection.query('INSERT INTO user (name, surname, email, password) VALUES (?,?,?,?)', [name, surname, email, password],(error, results) => {
+      if (error){
+        return res.status(500).json({ error: error });
+      }
+    });
+    res.sendStatus(200);
+  });
 
+  app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+  });
+}
 
-app.post('/add', (req, res) => {
-  const { name, surname, email, password } = req.query;
-
-  connection.query('INSERT INTO user (name, surname, email, password) VALUES (?,?,?,?)', [name, surname, email, password],(error, results) => {
-     if (error){
-      return res.status(500).json({ error: error });
-     }
-     });
-     res.sendStatus(200);
-
- 
-});
+connectWithRetry();
